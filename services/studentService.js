135bac72f3120186b6763e5543e1313f9ce49637
Anjana Samarakoon <anjana.dinethra@gmail.com>
2022-06-02 00:35:15 +0530

    student service created

---
+++ services/studentService.js
@@ -0,0 +1,53 @@
+import {
+    createStudent,
+    getStudents,
+    getStudentById,
+    updateStudent,
+    deleteStudent,
+} from "../repository/studentRepo.js";
+
+export const creteStudentService = async (data) => {
+    const { studentId, email, userName,contactNumber,password } = data;
+    try {
+        await createStudent({ studentId, email, userName,contactNumber,password });
+        return Promise.resolve("Student Saved");
+    } catch (err) {
+        throw new Error(err.message, err.status);
+    }
+};
+
+export const getStudentsService = async () => {
+    try {
+        const student = await getStudents();
+        return Promise.resolve(student);
+    } catch (err) {
+        throw new Error(err.message, err.status);
+    }
+};
+
+export const getStudentByIdService = async (studentId) => {
+    try {
+        const student = await getStudentById(studentId);
+        return Promise.resolve(student);
+    } catch (err) {
+        throw new Error(err.message, err.status);
+    }
+};
+
+export const updateStudentService = async (studentId, data) => {
+    try {
+        const student = await updateStudent(studentId, data);
+        return Promise.resolve(student);
+    } catch (err) {
+        throw new Error(err.message, err.status);
+    }
+};
+
+export const deleteStudentService = async (studentId) => {
+    try {
+        const student = await deleteStudent(studentId);
+        return Promise.resolve(student);
+    } catch (err) {
+        throw new Error(err.message, err.status);
+    }
+};
