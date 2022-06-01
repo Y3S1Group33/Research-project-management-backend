bd8cedacc60a426027f8d1222993ee6851e87567
Anjana Samarakoon <anjana.dinethra@gmail.com>
2022-06-02 00:29:32 +0530

    student repo added

---
+++ repository/studentRepo.js
@@ -0,0 +1,55 @@
+import { Student } from "../models/studentModel.js";
+
+export const createStudent = (data) =>
+    Student.create(data)
+        .then((Student) => {
+            return Promise.resolve(Student);
+        })
+        .catch(() => {
+            throw new Error("Internal server error.", 500);
+        });
+
+export const getStudentById = (studentId) =>
+    Student.findById(studentId)
+        .then((Student) => {
+            if (!Student) {
+                throw new Error("Students not found.", 404);
+            }
+            return Promise.resolve(Student);
+        })
+        .catch(() => {
+            throw new Error("Internal server error.", 500);
+        });
+
+export const getStudents = () =>
+    Student.find()
+        .then((Student) => {
+            return Promise.resolve(Student);
+        })
+        .catch(() => {
+            throw new Error("Internal server error.", 500);
+        });
+
+export const updateStudent = (id, data) =>
+    Student.findByIdAndUpdate(id, data, { new: true })
+        .then((Student) => {
+            if (!Student) {
+                throw new Error("Students not found.", 404);
+            }
+            return Promise.resolve(Student);
+        })
+        .catch(() => {
+            throw new Error("Internal server error.", 500);
+        });
+
+export const deleteStudent = (id) =>
+    Student.findByIdAndDelete(id)
+        .then((Student) => {
+            if (!Student) {
+                throw new Error("MarkingSchemas not found.", 404);
+            }
+            return Promise.resolve(Student);
+        })
+        .catch(() => {
+            throw new Error("Internal server error.", 500);
+        });
