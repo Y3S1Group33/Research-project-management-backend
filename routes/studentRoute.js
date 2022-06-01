bd43d4e55fa393a82b5843634a9fcf3e456f247e
Anjana Samarakoon <anjana.dinethra@gmail.com>
2022-06-02 00:33:16 +0530

    student routes added

---
+++ routes/studentRoute.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+    createController,
+    getStudentsController,
+    getStudentByIdController,
+    updateStudentController,
+    deleteStudentController,
+} from "../controllers/studentController.js";
+
+const studentRoutes = express.Router();
+
+
+
+studentRoutes.post("/student", createController);
+studentRoutes.get("/student", getStudentsController);
+studentRoutes.get("/student/:id", getStudentByIdController);
+studentRoutes.put("/student/:id", updateStudentController);
+studentRoutes.delete("/student/:id", deleteStudentController);
+
+export default studentRoutes;
