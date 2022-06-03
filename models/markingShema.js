92901ca9f862133a9bc8ed7ec2b6f8dd8506fbb5
uihalagedara <it20224820@my.sliit.lk>
2022-06-03 10:30:49 +0530

    marking schema model added

---
+++ models/markingShema.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const markingSchemaModel = new Schema(
+    {
+        title:String,
+        submittedDate:String,
+        imageUrl: {
+            type: String,
+            required: true,
+        }
+    }
+
+);
+
+export const markingSchemaSubmission = mongoose.model("markingSchemaSubmission",markingSchemaModel );
