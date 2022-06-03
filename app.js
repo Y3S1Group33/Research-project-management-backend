5b1b7e85dd296286cb233b93e4d33edcc8b5d896
uihalagedara <it20224820@my.sliit.lk>
2022-06-03 12:32:16 +0530

    env added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser"
 import multer from "multer";
+import "dotenv/config";
 import { connect } from "./utils/connection.js";
 
 import referenceRoutes from "./routes/reference.js";
@@ -23,7 +24,7 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 connect();
 
